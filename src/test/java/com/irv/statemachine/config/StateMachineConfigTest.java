a1f48126805e9fedc0662c8b033694738575d22a
irvcal <juanirvincalderon@gmail.com>
2022-03-25 14:31:59 -0600

    State machine test, se hizo la clase para hacer un test del flujo de los eventos

---
+++ src/test/java/com/irv/statemachine/config/StateMachineConfigTest.java
@@ -0,0 +1,34 @@
+package com.irv.statemachine.config;
+
+import com.irv.statemachine.domain.PaymentEvent;
+import com.irv.statemachine.domain.PaymentState;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class StateMachineConfigTest {
+    
+    @Autowired
+    StateMachineFactory<PaymentState, PaymentEvent> factory;
+
+    @Test
+    void testNewStateMachine() {
+        StateMachine<PaymentState,PaymentEvent> sm = factory.getStateMachine(UUID.randomUUID());
+        sm.start();
+        getState(sm);
+        sm.sendEvent(PaymentEvent.PRE_AUTHORIZE);
+        getState(sm);
+        sm.sendEvent(PaymentEvent.PRE_AUTHORIZE_APPROVED);
+        getState(sm);
+    }
+    void getState(StateMachine<PaymentState,PaymentEvent> sm){
+        System.out.println(sm.getState().toString());
+    }
+}
